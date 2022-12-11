6ce7cdf11133fc2b2349f4b2a3756e09ff3e9e2e
rahulnj~ <rahuljayaraman8@gmail.com>
2022-12-11 19:14:24 +0530

    feat: change in the hconnectertop

--- src/App.js
+++ src/App.js
@@ -7,13 +7,18 @@ const App=()=> {
     {/* <!-- Root node (Category) --> */}
     <li><div class="sticky">Animals</div>
       <ul>
-
         {/* <!-- Sub category --> */}
         <li><div class="sticky">Birds</div></li> 
         {/* <!-- Second level node --> */}
-        <li><div class="sticky">Mammals</div>
-        
-        </li>
+        <li><div class="sticky">Mammals</div></li>
+      </ul>
+    </li>
+
+    <li><div class="sticky">Plants</div>
+      <ul>
+        <li><div class="sticky">Flowers</div></li>
+        <li><div class="sticky">Trees</div></li>
+        <li><div class="sticky">Trees</div></li>
       </ul>
     </li>
 
