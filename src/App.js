dd831b8e0b86d1bff32f5054e03183982144d7c2
rahulnj~ <rahuljayaraman8@gmail.com>
2023-01-01 12:08:16 +0530

    feat: added the dom swap function

--- src/App.js
+++ src/App.js
@@ -1,38 +1,70 @@
+const App = () => {
+  const handleOnClick = () => {
+    const para = document.createElement('p')
+    const node = document.createTextNode('This is new.')
+    para.appendChild(node)
+    console.log(para);
+    const main = document.getElementsByClassName('app')[0];
+    // main.setAttribute('id','para-1');
+    
+    console.log(main);
+    const clone = para.cloneNode(true);
 
+    while (main.firstChild) main.firstChild.remove();
 
-const App=()=> {
+    main.appendChild(clone);
+  }
   return (
-<div>
-  <ul class="tree">
-    {/* <!-- Root node (Category) --> */}
-    <li><div class="sticky">Animals</div>
-      <ul>
-        {/* <!-- Sub category --> */}
-        <li><div class="sticky">Birds</div></li> 
-        {/* <!-- Second level node --> */}
-        <li><div class="sticky">Mammals</div></li>
-      </ul>
-    </li>
+    <div className='app'>
+      <ul className='tree'>
+        {/* <!-- Root node (Category) --> */}
+        <li>
+          <div className='sticky'>Animals</div>
+          <ul>
+            {/* <!-- Sub category --> */}
+            <li>
+              <div className='sticky'>Birds</div>
+            </li>
+            {/* <!-- Second level node --> */}
+            <li>
+              <div className='sticky'>Mammals</div>
+            </li>
+          </ul>
+        </li>
 
-    <li><div class="sticky">Plants</div>
-      <ul>
-        <li><div class="sticky">Flowers</div></li>
-        <li><div class="sticky">Trees</div></li>
-        <li><div class="sticky">Trees</div></li>
-      </ul>
-    </li>
+        <li>
+          <div className='sticky'>Plants</div>
+          <ul>
+            <li>
+              <div className='sticky'>Flowers</div>
+            </li>
+            <li>
+              <div className='sticky'>Trees</div>
+            </li>
+            <li>
+              <div className='sticky'>Trees</div>
+            </li>
+          </ul>
+        </li>
 
-    <li><div class="sticky">Plants</div>
-      <ul>
-        <li><div class="sticky">Flowers</div></li>
-        <li><div class="sticky">Trees</div></li>
-        <li><div class="sticky">Trees</div></li>
+        <li>
+          <div className='sticky'>Plants</div>
+          <ul>
+            <li>
+              <div className='sticky'>Flowers</div>
+            </li>
+            <li>
+              <div className='sticky'>Trees</div>
+            </li>
+            <li>
+              <div className='sticky'>Trees</div>
+            </li>
+          </ul>
+        </li>
       </ul>
-    </li>
-  </ul>
-</div>
-
-  );
+      <button onClick={handleOnClick}>click me</button>
+    </div>
+  )
 }
 
-export default App;
+export default App
